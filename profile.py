5306b1713362357638c7821214c97ce287faa59f
giannisp01 <gpanis01@ucy.ac.cy>
2024-04-24 10:50:09 +0300

    Initial Commit

---
+++ profile.py
@@ -0,0 +1,35 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+# Import the InstaGENI library.
+import geni.rspec.igext as ig
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
+
+# Create a portal object,
+pc = portal.Context()
+
+portal.Parameter(
+    "name", "VLAN Name",
+    portal.ParameterType.STRING, "",
+    longDescription="A shared VLAN name (functions as a private key allowing other experiments to connect to this VLAN). Must be fewer than 32 alphanumeric characters."),
+params = pc.bindParameters()
+
+pc.verifyParameters()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+tour = ig.Tour()
+tour.Description(ig.Tour.TEXT, "Create a shared vlan.")
+request.addTour(tour)
+
+sharedvlan = pg.Link('shared-vlan')
+sharedvlan.createSharedVlan(params.name)
+sharedvlan.link_multiplexing = True
+sharedvlan.best_effort = True
+
+request.addResource(sharedvlan)
+
+pc.printRequestRSpec(request)
